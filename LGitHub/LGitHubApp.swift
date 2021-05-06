aaeb9037346dd0f249b6a02df9db494bcefcf9ae
Stian Jacobsen <sj@spear.no>
2021-05-06 15:13:01 +0200

    Initial Commit

---
+++ LGitHub/LGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LGitHubApp.swift
+//  LGitHub
+//
+//  Created by Stian Jacobsen on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
